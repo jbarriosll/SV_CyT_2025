43153473a17aa213460bbff7d1a1c9466831e617
jbarriosll <jrbarrioslla@gmail.com>
2025-02-13 18:08:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Curso de Ciencia y Tecnología", layout="wide")
 
 # Cargar un logo pequeño en la parte superior
-logo_top_path = "logo_top.jpg"  # Asegúrate de subir este archivo al mismo directorio que el script
+logo_top_path = "SV_insignia.jpg"  # Asegúrate de subir este archivo al mismo directorio que el script
 st.image(logo_top_path, width=100)
 
 st.title("Curso de Ciencia y Tecnología")
